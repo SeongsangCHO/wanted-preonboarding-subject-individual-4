85d294a4fa29dc685f8df0d2d102322ea1bd37ce
Seongsangcho <liasqui31@gmail.com>
2021-09-01 03:43:34 +0900

    style : 공통 버튼 컴포넌트, shadow mixin, theme추가

---
+++ src/styles/mixin.ts
@@ -0,0 +1,6 @@
+import { css } from "styled-components";
+
+export const Shadow = () => css`
+  -webkit-box-shadow: 0px 1px 5px -2px rgba(0, 0, 0, 0.94);
+  box-shadow: 0px 1px 5px -2px rgba(0, 0, 0, 0.94);
+`;
