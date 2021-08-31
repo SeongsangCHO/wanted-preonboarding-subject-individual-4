e552bf22990865c4be19e0f9d8c8760e80f4b5d7
Seongsangcho <liasqui31@gmail.com>
2021-08-31 23:43:33 +0900

    feat : todoList, Item추가, 데이터 렌더링

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React from "react";
+import { ITodo } from "types/todo";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { requestCheckTodoItem, requestDeleteTodoItem } from "store/actions/todo";
 
-interface IProps {}
+interface IProps {
+  todo: ITodo;
+}
 
-const TodoItem: React.FC<IProps> = ({}) => {
-  return <div>item</div>;
+const TodoItem: React.FC<IProps> = ({ todo }) => {
+  const dispatch = useDispatch();
+  return (
+    <Container>
+      <LeftSide>
+        <button onClick={() => dispatch(requestCheckTodoItem(todo.id))}>체크박스</button>
+        {todo.content}
+      </LeftSide>
+      <RightSide>
+        <button>수정버튼</button>
+        <button onClick={() => dispatch(requestDeleteTodoItem(todo.id))}>삭제버튼버튼</button>
+      </RightSide>
+    </Container>
+  );
 };
 export default TodoItem;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid #827a7a;
+`;
+const LeftSide = styled.div``;
+const RightSide = styled.div``;
