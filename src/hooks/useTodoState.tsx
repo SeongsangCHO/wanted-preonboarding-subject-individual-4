4f8e08e6b17c3989924dc5a1531d4db8eb6465d6
Seongsangcho <liasqui31@gmail.com>
2021-08-31 23:42:48 +0900

    feat : 로컬스토리지, 전역스테이트 연동하기 위한 hooks생성

--- src/hooks/useTodoState.tsx
+++ src/hooks/useTodoState.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "store";
 
 const useTodoState = () => {
   const { todo: todoState } = useSelector((state: RootState) => state);
-  //dispatch(가져오기함수) -> state에 업데이트하는것까지만하기
-  // 그럼 밑에서 변화가 감지되므로 로컬로 저장하는 함수가 실행될거임
-  useEffect(() => {
-    //로컬에 저장하는 함수
-  }, [todoState]);
-  return { todoState };
+  const dispatch = useDispatch();
+  return { todoState, dispatch };
 };
 export default useTodoState;
